b8000c150687238918eaea385bb4972b71efb997
y-hrubskyi <129675742+y-hrubskyi@users.noreply.github.com>
2023-09-01 21:10:12 +0000

    Deploying to gh-pages from @ y-hrubskyi/goit-js-hw-09@b4570c17a13fe77a4d867b2552eae231d2105008 🚀

---
+++ html/01-color-switcher.8bcba8ee.js
@@ -0,0 +1,2 @@
+!function(){var t={startBtn:document.querySelector("button[data-start]"),stopBtn:document.querySelector("button[data-stop]")},n=null;t.startBtn.addEventListener("click",(function(){t.startBtn.disabled=!0,t.stopBtn.disabled=!1,n=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3)})),t.stopBtn.addEventListener("click",(function(){t.stopBtn.disabled=!0,t.startBtn.disabled=!1,clearInterval(n)}))}();
+//# sourceMappingURL=01-color-switcher.8bcba8ee.js.map
